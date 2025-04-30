f0d9426417db5133a48f789a92a622201df73e39
Charlesss-0 <aragonaugusto588@gmail.com>
2025-04-29 22:40:46 -0600

    feat: add progress indicator to goal info

--- src/components/goals/goalInfo.tsx
+++ src/components/goals/goalInfo.tsx
@@ -25,6 +25,7 @@ export default function GoalInfo({
 }): React.ReactNode {
 	const { goals } = useGoalStore()
 	const goal = goals.find(goal => goal.id === selectedGoalId)
+	const progress = goal && (goal.currentAmount / goal.targetAmount) * 100
 
 	return (
 		<Dialog open={open} onOpenChange={onOpenChange}>
@@ -48,8 +49,11 @@ export default function GoalInfo({
 							<span className="text-neutral">Priority</span>
 							<span>{capitalize(goal.priority)}</span>
 						</div>
-						<div>
+						<div className="flex flex-col gap-2">
 							<Progress value={(goal.currentAmount / goal.targetAmount) * 100} className="h-2" />
+							<span className="text-neutral text-sm text-center">
+								{progress?.toFixed(1)}% Complete
+							</span>
 						</div>
 					</>
 				)}
